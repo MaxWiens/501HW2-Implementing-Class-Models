9d72f6668deb2e110d8801e85e78f58984d66861
Jazz Loffredo <jazzloffredo@ksu-wireless-241-245.campus.ksu.edu>
2018-09-15 16:58:55 -0500

    Semester class finished

--- Semester.cs
+++ Semester.cs
@@ -1,13 +1,20 @@
 class Semester
 {
 	private string name;
-	
+	private List<Section> _sections;
+
 	public Semester(string name)
-	{}
+	{
+		this.name = name;
+	}
 	
 	public string GetName()
-	{}
+	{
+		return name;
+	}
 
 	public IncludeSection(Section newSection)
-	{}
+	{
+		_sections.Add(newSection);
+	}
 }
