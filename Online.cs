8484d2650ad2cdded4394d979d1a8792ad993262
Max Wiens-Evangelista <rothmoss@gmail.com>
2018-09-15 17:04:37 -0500

    added classes

--- Online.cs
+++ Online.cs
@@ -1,5 +1,11 @@
-class Online
+class Online : Section
 {
 	public Online(Semester semester, Course course, Faculty teacher, string number, int cap)
-	{}
+	{
+        _semester = semester;
+        _course = course;
+        _faculty = teacher;
+        _number = number;
+        _cap = cap;
+    }
 }
--- Section.cs
+++ Section.cs
@@ -1,12 +1,15 @@
-abstract class OnCampus
+
+abstract class Section
 {
-	public string _number;
-	public int _cap;
-	private Semester _sections;
-	private IFaculty _faculty;
-	private Course _course;
-	public void SetNumber(string number);
-	public string GetNumber();
-	public void SetCap(int cap);
-	public int GetCap();
+    public string _number;
+    public int _cap;
+
+    protected Semester _semester;
+    protected IFaculty _faculty;
+    protected Course _course;
+
+    public void SetNumber(string number) => _number = number;
+    public string GetNumber() => _number;
+    public void SetCap(int cap) => _cap = cap;
+    public int GetCap() => _cap;
 }
