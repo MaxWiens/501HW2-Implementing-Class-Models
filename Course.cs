fd7fccaf02883dfba877f8af19494118daea637c
Jazz Loffredo <jazzloffredo@ksu-wireless-241-245.campus.ksu.edu>
2018-09-15 17:02:14 -0500

    Course class finished

--- Course.cs
+++ Course.cs
@@ -2,4 +2,12 @@ class Course
 {
 	public string Title;
 	public int Number;
+	/*
+	private List<Section> _sections;
+
+	public IncludeSection(Section newSection)
+	{
+		_sections.Add(newSection);
+	}
+	*/
 }
